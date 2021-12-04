da936508a2503a7e0099fcc46b6c3d0eea5e17be
klem <ddoherty6@gmail.com>
2021-12-03 23:17:22 -0800

    finished static styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,13 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do")); //display current day in header
+
+    
+var timeStyling = function(currentTime) {
+    descriptionEls = $("textarea");
+
+    descriptionEls.array.forEach(element => {
+        
+        
+    });
+
+    
+}
