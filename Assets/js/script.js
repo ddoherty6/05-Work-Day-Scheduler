dfaa5c86e1f5a2ff354026ed305e225deb3d29eb
Daniel <ddoherty6@gmail.com>
2021-12-04 15:27:49 -0800

    started stylePage object and storage object

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,57 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do")); //display current day in header
 
     
-var timeStyling = function(currentTime) {
-    descriptionEls = $("textarea");
+var style = {
+    stylePage: function(currentTime) {
+        var descriptionEls = $("textarea");
 
-    descriptionEls.array.forEach(element => {
-        
-        
-    });
+        for (var i = 0; i < descriptionEls.length; i++) {
+            if (currentTime < ) {
 
-    
+            } else if (currentTime === ) {
+
+            } else if (currentTime > ) {
+
+            }
+            
+        };
+
+        
+    },
+
+    clock: function() {
+        this.stylePage(moment.hour()); // style the page 
+        this.nexTime();
+    },
+
+    nexTime: function() {
+
+        var minUntilNextTime = 60 - moment.minute(); // how many minutes until the next hour starts
+
+        setTimeout(function() {
+            this.clock(); //do it all over again
+        }, minUntilNextTime*1000+5000); // adding 5 seconds to timer to be sure stylePage() runs in the correct hour
+    }
+}
+
+var storeAppt = {
+
+    appointments: Array(9),
+    apptStorage: window.localStorage(),
+
+    saveAppt: function() {
+
+    },
+
+    loadAppts: function() {
+
+        for (var i = 0; i < this.apptStorage.length; i++) {
+            
+        }
+
+    }
+
+
+
+
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# 05-Work-Day-Scheduler
