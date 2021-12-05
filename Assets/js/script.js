c287aba26e7c4da158528b7cf5860f5a9a5acd5d
klem <ddoherty6@gmail.com>
2021-12-04 19:51:06 -0800

    finished save function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,53 +1,76 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do")); //display current day in header
-
     
-var style = {
-    stylePage: function(currentTime) {
-        var descriptionEls = $("textarea");
 
-        for (var i = 0; i < descriptionEls.length; i++) {
-            if (currentTime < ) {
+var setColors = { // includes time methods and styling methods
+    neededStyles: Array(9),
+    textAreas: $("textarea"),
 
-            } else if (currentTime === ) {
-
-            } else if (currentTime > ) {
-
+    determineStyles: function(currentTime) {
+        var times = [9,10,11,12,13,14,15,16,17];
+       
+        for (var i = 0; i < times.length; i++) { 
+            if (times[i] > currentTime) {
+                this.neededStyles[i] = "future";
+            } else if (times[i] === currentTime) {
+                this.neededStyles[i] = "present";                
+            } else {
+                this.neededStyles[i] = "past";
             }
-            
-        };
+        }        
+    },
 
-        
+    stylePage: function(styles) {
+
+        for (var i= 0; i < styles.length; i++) {
+            this.textAreas[i].classList.add(styles[i]);
+        }
+
+    },
+
+    driver: function() {
+        this.determineStyles(moment().hour()); // determine the styles needed
+        this.stylePage(this.neededStyles); // feed needed styles into styler func 
+        this.clock(); //call function that determines how long to wait until call driver() again
     },
 
     clock: function() {
-        this.stylePage(moment.hour()); // style the page 
-        this.nexTime();
-    },
 
-    nexTime: function() {
+        var minUntilNextTime = 60 - moment().minute(); // how many minutes until the next hour starts
 
-        var minUntilNextTime = 60 - moment.minute(); // how many minutes until the next hour starts
 
         setTimeout(function() {
-            this.clock(); //do it all over again
-        }, minUntilNextTime*1000+5000); // adding 5 seconds to timer to be sure stylePage() runs in the correct hour
+            setColors.driver(); //do it all over again
+        }, minUntilNextTime*1000+5000); // adding 5 seconds to timer to be sure determineStyles() runs in the correct hour
     }
 }
 
-var storeAppt = {
+var apptStorage = {
 
     appointments: Array(9),
-    apptStorage: window.localStorage(),
 
-    saveAppt: function() {
+    saveAppt: function(ID) {
+        var text = $("#"+ID).find("textarea").val(); // find child textarea of article with give ID, pull contents of that textarea 
+        console.dir(text);
+
+        localStorage.setItem(ID, text);
+
+
 
     },
 
     loadAppts: function() {
 
-        for (var i = 0; i < this.apptStorage.length; i++) {
+        for (var i = 0; i < this.appointments.length; i++) {
             
         }
+
+    },
+
+    clickHandler: function(event) {
+        var selectedElID = $(event.target).parents(".row").attr("id"); // find the parent article and get it's id
+
+        apptStorage.saveAppt(selectedElID); // pass article ID to saveAppt method
+        
 
     }
 
@@ -55,3 +78,8 @@ var storeAppt = {
 
 
 }
+
+
+setColors.driver();
+
+$("article").on("click", "button", apptStorage.clickHandler);
