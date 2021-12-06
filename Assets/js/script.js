a39a04215067df77f05d7e00a432f6f24133271b
klem <ddoherty6@gmail.com>
2021-12-05 19:49:55 -0800

    ready for merge

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,12 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do")); //display current day in header
 
-var timer = null;
+var timer = null; // global timer for clock() method
 
 var setColors = { // includes time methods and styling methods
     neededStyles: Array(9),
     textAreas: $("textarea"),
 
-    determineStyles: function(currentTime) {
+    determineStyles: function(currentTime) { // compare currentTime to times listed on page; determine past, present or future
         var times = [9,10,11,12,13,14,15,16,17];
        
         for (var i = 0; i < times.length; i++) { 
@@ -23,17 +23,17 @@ var setColors = { // includes time methods and styling methods
     stylePage: function(styles) {
 
         $("textarea").removeAttr("class"); // reset all textarea class attributes
-        $("textarea").addClass("col-10"); // add back in bootstrap class
+        $("textarea").addClass("col-10"); // add back in bootstrap class to all textarea elements
 
         for (var i= 0; i < styles.length; i++) {
-            this.textAreas[i].classList.add(styles[i]); // add class styles based on those determined
+            this.textAreas[i].classList.add(styles[i]); // add class styles based on those determined above
         }
     },
 
     driver: function() {
         
-        this.determineStyles(moment().hour()); // determine the styles needed
-        this.stylePage(this.neededStyles); // feed needed styles into styler func 
+        this.determineStyles(moment().hour()); // determine the styles needed based on current time
+        this.stylePage(this.neededStyles); // feed needed styles into stylePage() method 
         this.clock(); //call function that determines how long to wait until call driver() again
     },
 
@@ -49,20 +49,19 @@ var setColors = { // includes time methods and styling methods
     }
 }
 
-var apptStorage = { // controls how the 'appointments' are store in localStorage
+var apptStorage = { // controls how the 'appointments' are stored in localStorage
 
     saveAppt: function(ID) {
-        var text = $("#"+ID).find("textarea").val(); // find child textarea of article with give ID, pull contents of that textarea 
-        console.dir(text);
+        var text = $("#"+ID).find("textarea").val(); // find child textarea of article with given ID, pull contents of that textarea 
 
-        localStorage.setItem(ID, text);
+        localStorage.setItem(ID, text); // add key/value pair to local storage
     },
 
     loadAppts: function() {
     
         for (var i = 0; i < $("article").length ; i++) {
 
-            articleID = $("article")[i].id; //get id of article at i
+            articleID = $("article")[i].id; //get id of article at [i]
 
             appointment = localStorage.getItem(articleID); // find value for key 'articleID' in localStorage
 
@@ -72,7 +71,7 @@ var apptStorage = { // controls how the 'appointments' are store in localStorage
     },
 
     clickHandler: function(event) {
-        var selectedElID = $(event.target).parents(".row").attr("id"); // find the parent article of button and get it's id
+        var selectedElID = $(event.target).parents(".row").attr("id"); // find the parent article of clicked 'button' and get it's id
 
         apptStorage.saveAppt(selectedElID); // pass article ID to saveAppt method
     }
